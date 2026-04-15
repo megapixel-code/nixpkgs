e684acff638d8243418c87dd0a73ce2edc1ea48a
megapixel-code <chainemegapixel@gmail.com>
2026-04-15 12:03:49 +0200

    feat(pasls): trying env vars

--- pkgs/by-name/pa/pascal-language-server/package.nix
+++ pkgs/by-name/pa/pascal-language-server/package.nix
@@ -57,8 +57,8 @@ stdenv.mkDerivation (finalAttrs: {
     cp src/standard/pasls $out/opt/pasls
     makeWrapper $out/opt/pasls $out/bin/pasls \
       --prefix FPCDIR : ${lazarus}/share/fpcsrc \
-      --prefix LAZARUSDIR: ${lazarus}/share/lazarus
-      # --prefix PP : ${fpc}/bin/ppcx64
+      --prefix LAZARUSDIR : ${lazarus}/share/lazarus \
+      --prefix PP : ${fpc}/bin/ppcx64
   '';
 
   meta = {
