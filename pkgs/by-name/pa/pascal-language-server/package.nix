718eb00fcf244a5d1a64aa0ddf4663b199b2eac6
megapixel-code <chainemegapixel@gmail.com>
2026-04-15 03:44:05 +0200

    fix(pascal-language-server): removed makeWrapper

--- pkgs/by-name/pa/pascal-language-server/package.nix
+++ pkgs/by-name/pa/pascal-language-server/package.nix
@@ -50,9 +50,9 @@ stdenv.mkDerivation (finalAttrs: {
     cp src/standard/pasls             $out/bin/pasls
 
     # cp src/standard/pasls $out/opt/pasls
-    makeWrapper $out/opt/pasls $out/bin/pasls \
-      --prefix PP : ${fpc}/bin/ppcx64 \
-      --prefix FPCDIR : ${fpc}
+    # makeWrapper $out/opt/pasls $out/bin/pasls \
+    #   --prefix PP : ${fpc}/bin/ppcx64 \
+    #   --prefix FPCDIR : ${fpc}
   '';
 
   meta = {
